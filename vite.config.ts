6bb645d14c592b67f53ac71319b35715f97cba95
mukles <muklesdev@gmail.com>
2025-09-26 22:56:34 +0600

    feat: add build configuration to specify output directory in Vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "dist",
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),

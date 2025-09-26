f177d289d842495302e7ba543737904a7263a0d0
mukles <muklesdev@gmail.com>
2025-09-26 22:12:57 +0600

    feat: add Toaster component with theme support; update AppLayout to include Toaster; add next-themes and sonner dependencies

--- src/app/layout/app-layout.tsx
+++ src/app/layout/app-layout.tsx
@@ -1,9 +1,11 @@
+import { Toaster } from "@/shared/components/ui/sonner";
 import { Outlet } from "@tanstack/react-router";
 
 export function AppLayout() {
   return (
     <div>
       <Outlet />
+      <Toaster />
     </div>
   );
 }
